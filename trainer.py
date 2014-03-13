b07002422a5cc2de54d0cd157ee3904d5b50fa86
MrValdez <shunyvaldez@gmail.com>
2014-03-13 14:41:23 +0800

    added trainer program

---
+++ trainer.py
@@ -0,0 +1,16 @@
+import subprocess
+
+filebase = "./results/"
+
+for variable in range(0, 1000):
+    filename = filebase + "results %s.txt" % (str(variable).zfill(4))
+    #print (filename)
+    
+    with open(filename, "w") as f:
+        output = subprocess.check_output(["a.exe", str(variable)], universal_newlines = True)
+        stdout = str(output)
+        f.write(stdout)
+    
+    print("%s is done." % (filename))
+    #input()
+    
