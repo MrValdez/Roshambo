e26fd32028d0859133b5a6acb576c6407b0f6ec1
MrValdez <shunyvaldez@gmail.com>
2014-04-10 23:04:31 +0800

    merged getRank.py and trainer.py

--- trainer.py
+++ trainer.py
@@ -1,16 +1,56 @@
+import os
 import subprocess
 
 filebase = "./results/"
 
-for variable in range(0, 1000):
-    filename = filebase + "results %s.txt" % (str(variable).zfill(4))
-    #print (filename)
+def main():
+    PlayTournament(1000)
+    CreateCSV("results3.csv")
+
+def PlayTournament(size):
+    for argv in range(size):
+        filename = filebase + "results %s.txt" % (str(argv).zfill(4))
+        print ("Running %s..." % (filename), end='')
+        
+        output = subprocess.check_output(["go.exe", str(argv)], universal_newlines = True)
+      
+        with open(filename, "w") as f:
+            stdout = str(output)
+            f.write(stdout)
+        
+        print("done")
+
+def CreateCSV(outputFilename = "results.csv"):
+    """Create CSV by looking at the rank of the Yomi AI"""
+    csv = ""
+    ranks = []
+    fileList = sorted(os.listdir(filebase))
     
-    with open(filename, "w") as f:
-        output = subprocess.check_output(["go.exe", str(variable)], universal_newlines = True)
-        stdout = str(output)
-        f.write(stdout)
+    print("VARIABLE    RANK")
+    for filename in fileList:
+        if filename[-4:] != ".txt":
+            print("%s is not txt file" % (filename))
+        
+        variable = filename[-8:-4]]
     
-    print("%s is done." % (filename))
-    #input()
-    
+        with open(filename) as f:
+            # find the rank of the Yomi AI            
+            text = f.read()
+            found = text.rfind("Yomi AI")
+            if found == -1:
+                print("MAJOR PARSING ERROR")
+                break
+                
+            start = text.rfind("\n", 0, found - 4)
+            end = text.find("Yomi AI", found)
+            
+            rank = str(text[start:end].strip())
+            ranks.append(rank)
+            
+            print (" %s        %s" % (variable, rank))            
+            csv += "%s,%s\n" % (variable, rank)
+
+    with open(outputFilename, "w") as f:
+        f.write(csv)
+        
+main()
