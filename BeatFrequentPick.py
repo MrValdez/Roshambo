8a66631fa805fcc7612b9771b156a2d86abc456b
MrValdez <shunyvaldez@gmail.com>
2014-04-11 08:25:00 +0800

    separated bfp to its own python script. added script for comparing between c and python

---
+++ BeatFrequentPick.py
@@ -0,0 +1,83 @@
+import rps
+from rps import biased_roshambo
+
+statR = 0
+statP = 0
+statS = 0
+
+denominator = 0
+
+rockProb    = 0.0
+paperProb   = 0.0
+targetRock  = 0.0
+targetPaper = 0.0
+
+Debug = False
+
+def move(targetTurn):
+    global statR, statP, statS
+    global denominator
+    global rockProb, paperProb, targetRock, targetPaper
+
+    currentTurn = rps.myHistory(0)
+    if (currentTurn == 0):
+        #initialize
+        statR = 0
+        statP = 0
+        statS = 0
+        denominator = targetTurn
+        rockProb = 1 / 3.0
+        paperProb = 1 / 3.0
+        targetRock = rockProb * targetTurn
+        targetPaper = paperProb * targetTurn
+        return biased_roshambo (1 / 3.0, 1 / 3.0)
+    
+    oppMove = rps.enemyHistory(currentTurn)
+    
+    if (oppMove == 0):
+        statR += 1
+    if (oppMove == 1):
+        statP += 1
+    if (oppMove == 2):
+        statS += 1
+
+    if (denominator <= 0):
+        rockProb = statR / float(currentTurn)
+        paperProb = statP / float(currentTurn)
+        
+        targetRock = float(rockProb * (currentTurn + targetTurn))
+        targetPaper = float(paperProb * (currentTurn + targetTurn))
+        
+        denominator = targetTurn
+        
+    thisTurnRockProb = float((targetRock - statR) / float(denominator))
+    thisTurnPaperProb = float((targetPaper - statP) / float(denominator))
+    
+    if Debug:
+        print("thisTurnRockProb, thisTurnPaperProb: %f %f" % (thisTurnRockProb, thisTurnPaperProb))
+        
+    if (thisTurnRockProb <= 0 or thisTurnPaperProb <= 0 or thisTurnRockProb + thisTurnPaperProb >= 1.0):
+        rockProb = statR / float (currentTurn)
+        paperProb = statP / float (currentTurn)
+        
+        targetRock = float(rockProb * (currentTurn + targetTurn))
+        targetPaper = float(paperProb * (currentTurn + targetTurn))
+        
+        denominator = targetTurn
+
+        thisTurnRockProb = float((targetRock - statR) / float(denominator))
+        thisTurnPaperProb = float((targetPaper - statP) / float(denominator))
+
+    thisTurnRockProb = 0 if thisTurnRockProb < 0 else thisTurnRockProb
+    thisTurnPaperProb = 0 if thisTurnPaperProb < 0 else thisTurnPaperProb
+    
+    if Debug:
+        # Debug
+        print("statR, targetRock: %i %f" % (statR, targetRock));
+        print("statP, targetPaper: %i %f" % (statP, targetPaper));
+        print("currentTurn, denominator: %i %i" % (currentTurn, denominator))
+        print("thisTurnRockProb, thisTurnPaperProb: %f %f\n" % (thisTurnRockProb, thisTurnPaperProb))
+        #input()
+        
+    denominator -= 1
+    return (biased_roshambo (thisTurnRockProb, thisTurnPaperProb) + 1) % 3
---
+++ compare.bat
@@ -0,0 +1,15 @@
+@echo off
+call compile.bat > temp/py1.txt
+echo done compiling
+
+go.exe 1 0 > temp/c1.txt
+echo variable 1 done
+
+go.exe 100 > temp/py100.txt
+echo python variable 100 done
+
+go.exe 100 0 > temp/c100.txt
+
+rem compare
+diff temp/c1.txt temp/py1.txt | less
+diff temp/c100.txt temp/py100.txt | less
