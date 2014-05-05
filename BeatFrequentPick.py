03436c4a253383cfd4453d7c5ae25fecbb644d5e
MrValdez <shunyvaldez@gmail.com>
2014-05-05 12:05:59 +0800

    refactored BFP to reflect paper

--- BeatFrequentPick.py
+++ BeatFrequentPick.py
@@ -12,8 +12,31 @@ paperProb   = 0.0
 targetRock  = 0.0
 targetPaper = 0.0
 
-Debug = True
 Debug = False
+Debug = True
+
+def calculateProbThisTurn():
+    global statR, statP, statS
+    global denominator
+    global rockProb, paperProb, targetRock, targetPaper
+
+    thisTurnRockProb = float((targetRock - statR) / float(denominator))
+    thisTurnPaperProb = float((targetPaper - statP) / float(denominator))
+    return (thisTurnRockProb, thisTurnPaperProb)
+
+def recompute(currentTurn, targetTurn):
+    global statR, statP, statS
+    global denominator
+    global rockProb, paperProb, targetRock, targetPaper
+
+    rockProb = statR / float (currentTurn)
+    paperProb = statP / float (currentTurn)
+    
+    targetRock = float(rockProb * (currentTurn + targetTurn))
+    targetPaper = float(paperProb * (currentTurn + targetTurn))
+    
+    denominator = targetTurn
+
 
 def BFP(targetTurn):
     global statR, statP, statS
@@ -22,15 +45,21 @@ def BFP(targetTurn):
 
     currentTurn = rps.getTurn()
     if (currentTurn == 0):
-        #initialize
-        statR = 0
-        statP = 0
-        statS = 0
+        #initialize        
+        statR, statP, statS = 0, 0, 0
         denominator = targetTurn
         rockProb = 1 / 3.0
         paperProb = 1 / 3.0
         targetRock = rockProb * targetTurn
         targetPaper = paperProb * targetTurn
+
+        if Debug:
+            print("\n\nInitial data (Target Turn %i):\n" % (targetTurn))
+            print("statR, statP: %i %i" % (statR, statP));
+            print("targetRock, targetPaper: %f %f" % (targetRock, targetPaper));
+            print("currentTurn, denominator: %i %i" % (currentTurn, denominator))
+            input()
+
         return biased_roshambo (1 / 3.0, 1 / 3.0)
     
     oppMove = rps.enemyHistory(currentTurn)
@@ -43,45 +72,49 @@ def BFP(targetTurn):
         statS += 1
 
     if (denominator <= 0):
-        rockProb = statR / float(currentTurn)
-        paperProb = statP / float(currentTurn)
+        if Debug:
+            print ("***********************")
+            print ("Recomputing denominator")
+            print ("***********************")
+            
+        recompute(currentTurn, targetTurn)
         
-        targetRock = float(rockProb * (currentTurn + targetTurn))
-        targetPaper = float(paperProb * (currentTurn + targetTurn))
-        
-        denominator = targetTurn
-        
-    thisTurnRockProb = float((targetRock - statR) / float(denominator))
-    thisTurnPaperProb = float((targetPaper - statP) / float(denominator))
+    thisTurnRockProb, thisTurnPaperProb = calculateProbThisTurn()
     
     if Debug:
+        print("currentTurn, denominator: %i %i" % (currentTurn, denominator))
+        print("statR, statP: %i %i" % (statR, statP));
+        print("probR, probP:                        %f %f" % (rockProb, paperProb));
+        print("targetRock, targetPaper:             %f %f" % (targetRock, targetPaper));
         print("thisTurnRockProb, thisTurnPaperProb: %f %f" % (thisTurnRockProb, thisTurnPaperProb))
+        debugRecomputation = False
         
     if (thisTurnRockProb <= 0 or thisTurnPaperProb <= 0 or thisTurnRockProb + thisTurnPaperProb >= 1.0):
-        rockProb = statR / float (currentTurn)
-        paperProb = statP / float (currentTurn)
-        
-        targetRock = float(rockProb * (currentTurn + targetTurn))
-        targetPaper = float(paperProb * (currentTurn + targetTurn))
-        
-        denominator = targetTurn
-
-        thisTurnRockProb = float((targetRock - statR) / float(denominator))
-        thisTurnPaperProb = float((targetPaper - statP) / float(denominator))
+        if Debug:
+            debugRecomputation = True
+            print ("***********************")
+            print ("Recomputing because of", thisTurnRockProb <= 0, thisTurnPaperProb <= 0, thisTurnRockProb + thisTurnPaperProb >= 1.0)
+            print ("***********************")
+            
+        recompute(currentTurn, targetTurn)
+        thisTurnRockProb, thisTurnPaperProb = calculateProbThisTurn()
 
     thisTurnRockProb = 0 if thisTurnRockProb < 0 else thisTurnRockProb
     thisTurnPaperProb = 0 if thisTurnPaperProb < 0 else thisTurnPaperProb
     
-    if Debug:
-        # Debug
-        print("statR, targetRock: %i %f" % (statR, targetRock));
-        print("statP, targetPaper: %i %f" % (statP, targetPaper));
-        print("currentTurn, denominator: %i %i" % (currentTurn, denominator))
+    if Debug and debugRecomputation:
+        print("targetRock, targetPaper: %f %f" % (targetRock, targetPaper));
         print("thisTurnRockProb, thisTurnPaperProb: %f %f\n" % (thisTurnRockProb, thisTurnPaperProb))
+    
+    if Debug:
         input()
         
     denominator -= 1
     return biased_roshambo (thisTurnRockProb, thisTurnPaperProb)
 
 def play(targetTurn):
-    return (BFP(targetTurn) + 1) % 3
+    move = BFP(targetTurn)
+    playing = (move + 1) % 3
+    if Debug:
+        print("Predicting %i. Playing %i\n\n"  % (move,playing))
+    return playing
