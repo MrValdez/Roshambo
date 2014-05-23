3395fdf6512847ddecf0dc26fc78670044f9b21a
MrValdez <shunyvaldez@gmail.com>
2014-05-23 10:39:05 +0800

    final graph design decided on

--- charts.py
+++ charts.py
@@ -13,13 +13,13 @@ def Plot(filename, title, saveFigure = True):
 
     # 5x4 inches
     fig = plt.figure(figsize=(10, 6), dpi=80, frameon = False)
-    fig.subplots_adjust(left=0.0, right=1.0, top=0.9, bottom=0.1, wspace=0, hspace=0)
+    fig.subplots_adjust(left=0.05, right=1.0, top=0.9, bottom=0.1, wspace=0, hspace=0)
 
     xMargin = 30
     yMargin = 2
     
     plt.xlim(-xMargin,1000+xMargin)
-    plt.ylim(int(min(y_data)) - yMargin, int(max(y_data)) + yMargin)
+    plt.ylim(0, int(max(y_data)) + yMargin)
     
     #plt.yticks(numpy.linspace(-1, 1, 5, endpoint=True))
     
@@ -28,7 +28,7 @@ def Plot(filename, title, saveFigure = True):
     #y_data = numpy.random.normal(5.0, 3.0, 1000)
     y_data = numpy.array(y_data)
     #ax.scatter(x_data, y_data, color="red", marker="^")
-    ax.plot(x_data, y_data, "bo", linewidth=0.1, linestyle="-")
+    ax.plot(x_data, y_data, "b", linewidth=1.0, linestyle="-")
     
     ax.set_title(title + "\n(lower is better)")
     ax.set_xlabel("targetTurn")
