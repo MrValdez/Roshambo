449c711e3d76781b196ab5a881c997995d34f8be
MrValdez <shunyvaldez@gmail.com>
2014-09-10 08:12:31 +0800

    updated charts for seqPredictor

--- charts.py
+++ charts.py
@@ -8,40 +8,55 @@ def Plot(filename, title, saveFigure = True):
 
     data = data.split("\n")
         
-    x_data = []
+    x_data_raw = []
     y_data = []
     for s in data:
         mid = s.rfind(",")
         row = [s[:mid], s[mid+1:]]
-        x_data.append(row[0])
-        y_data.append(row[1])
+        x_data_raw.append(row[0])
+        y_data.append(int(row[1]))
+        
+    x_data_raw.reverse()
+    y_data.reverse()
 
     # 5x4 inches
-    fig = plt.figure(figsize=(10, 6), dpi=80, frameon = False)
-    fig.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.1, wspace=0, hspace=0)
+    fig = plt.figure(figsize=(15, 6), dpi=80, frameon = False)
+    fig.subplots_adjust(left=0.05, right=0.55, top=0.9, bottom=0.1, wspace=0, hspace=0)
 
     xMargin = 2
-    yMargin = 2
-    
+    yMargin = 3
+
+    y_data = numpy.array(y_data)    
     plt.xlim(-xMargin,len(y_data)+xMargin)
     plt.ylim(0, int(max(y_data)) + yMargin)
     
-    #plt.yticks(numpy.linspace(-1, 1, 5, endpoint=True))
+    plt.yticks(y_data)
     
     ax = fig.add_subplot(1,1,1) # one row, one column, first plot
 
-    #y_data = numpy.random.normal(5.0, 3.0, 1000)
-    y_data = numpy.array(y_data)
     #ax.scatter(x_data, y_data, color="red", marker="^")
     #ax.plot(x_data, y_data, "b", linewidth=1.0, linestyle="-")
-    #ax.plot([s for s in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:len(y_data)]], y_data, "b", linewidth=1.0, linestyle="-")
-    ax.plot(range(len(y_data)), y_data, "b", linewidth=1.0, linestyle="-")
+    x_data = range(len(y_data))
+    alphabet = [s for s in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:len(y_data)]]
+    plt.xticks(x_data, alphabet, rotation=0)
+    ax.plot(x_data, y_data, "bo", linewidth=1.0, linestyle="-")
     
     #ax.set_title(title + "\n(lower is better)")
     #ax.set_title("(lower is better)")
     ax.set_xlabel("SequenceSize")
     ax.set_ylabel("Ranking")
 
+    #textstr = '$\mu=%.2f$\n$\mathrm{median}=%.2f$\n$\sigma=%.2f$'%(4.2, 2.1, 2.0)
+    
+    textstr = ""
+    for i, data in enumerate(x_data_raw):
+        textstr +=  "%s: %s\n" % (alphabet[i], data)
+
+    # these are matplotlib.patch.Patch properties
+    props = dict(boxstyle='round', facecolor='white')
+
+    ax.text(1.02, 0.99, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+        
     if saveFigure:
         figureName = str(filename).split(".")[0] + ".png"
         fig.savefig(figureName)
@@ -50,10 +65,10 @@ def Plot(filename, title, saveFigure = True):
         fig.show()
 
 def startPlotting():
-    #Plot("results_match.csv", "Match Results", True)
-    #Plot("results_tournament.csv", "Tournament Results", True)    
-    Plot("results_match.csv", "Match Results", False)
-    #Plot("results_tournament.csv", "Tournament Results", True)    
+    Plot("results_match.csv", "Match Results", True)
+    Plot("results_tournament.csv", "Tournament Results", True)    
+    #Plot("results_match.csv", "Match Results", False)
+    #Plot("results_tournament.csv", "Tournament Results", False)    
 
 if __name__ == "__main__":
     startPlotting()
