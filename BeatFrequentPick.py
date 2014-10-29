8e840d87304f5f55fca0f0ac4fd28f1493f92583
MrValdez <shunyvaldez@gmail.com>
2014-10-29 16:47:36 +0800

    moved MBFP into class

--- BeatFrequentPick.py
+++ BeatFrequentPick.py
@@ -1,128 +1,121 @@
 import rps
 from rps import biased_roshambo
 
-statR = 0
-statP = 0
-statS = 0
-
-remainingPredictionSize = 0
-
-probR = 0.0
-probP = 0.0
-predictionR = 0.0
-predictionP = 0.0
-
 Debug = True
 Debug = False
 
-def calculateProbThisTurn():
-    global statR, statP, statS
-    global remainingPredictionSize
-    global probR, probP, predictionR, predictionP
+class MBFP:
+    def __init__(self):
+        self.reset()
+        
+    def reset(self):
+        self.statR = 0
+        self.statP = 0
+        self.statS = 0
 
-    thisTurnProbR = float((predictionR - statR) / float(remainingPredictionSize))
-    thisTurnProbP = float((predictionP - statP) / float(remainingPredictionSize))
-    
-    return (thisTurnProbR, thisTurnProbP)
+        self.remainingPredictionSize = 0
 
-def recomputeFutureProb(currentTurn, targetPredictionSize):
-    global statR, statP, statS
-    global remainingPredictionSize
-    global probR, probP, predictionR, predictionP
+        self.probR = 0.0
+        self.probP = 0.0
+        self.predictionR = 0.0
+        self.predictionP = 0.0
 
-    probR = statR / float (currentTurn)
-    probP = statP / float (currentTurn)
-    
-    predictionR = float(probR * (currentTurn + targetPredictionSize))
-    predictionP = float(probP * (currentTurn + targetPredictionSize))
-    
-    remainingPredictionSize = targetPredictionSize
+    def calculateProbThisTurn(self):
+        thisTurnProbR = float((self.predictionR - self.statR) / float(self.remainingPredictionSize))
+        thisTurnProbP = float((self.predictionP - self.statP) / float(self.remainingPredictionSize))
+        
+        return (thisTurnProbR, thisTurnProbP)
 
+    def recomputeFutureProb(self, currentTurn, targetPredictionSize):
+        self.probR = self.statR / float (currentTurn)
+        self.probP = self.statP / float (currentTurn)
+        
+        self.predictionR = float(self.probR * (currentTurn + targetPredictionSize))
+        self.predictionP = float(self.probP * (currentTurn + targetPredictionSize))
+        
+        self.remainingPredictionSize = targetPredictionSize
 
-def BFP(targetPredictionSize):
-    global statR, statP, statS
-    global remainingPredictionSize
-    global probR, probP, predictionR, predictionP
+    def predict(self, targetPredictionSize):
+        currentTurn = rps.getTurn()
+        if (currentTurn == 0):
+            #initialize        
+            self.statR, self.statP, self.statS = 0, 0, 0
+            self.remainingPredictionSize = targetPredictionSize
+            self.probR = 1 / 3.0
+            self.probP = 1 / 3.0
+            self.predictionR = self.probR * targetPredictionSize
+            self.predictionP = self.probP * targetPredictionSize
 
-    currentTurn = rps.getTurn()
-    if (currentTurn == 0):
-        #initialize        
-        statR, statP, statS = 0, 0, 0
-        remainingPredictionSize = targetPredictionSize
-        probR = 1 / 3.0
-        probP = 1 / 3.0
-        predictionR = probR * targetPredictionSize
-        predictionP = probP * targetPredictionSize
+            if Debug:
+                print("\n\nInitial data (Target Prediction Size %i):\n" % (self.targetPredictionSize))
+                print("statR, statP:                         %i    %i" % (self.statR, self.statP));
+                print("predictionR, predictionP:             %.2f %.2f" % (self.predictionR, self.predictionP));
+                print("currentTurn, remainingPredictionSize: %i    %i" % (currentTurn, self.remainingPredictionSize))
+                print("Done init()\n")
 
-        if Debug:
-            print("\n\nInitial data (Target Prediction Size %i):\n" % (targetPredictionSize))
-            print("statR, statP:                         %i    %i" % (statR, statP));
-            print("predictionR, predictionP:             %.2f %.2f" % (predictionR, predictionP));
-            print("currentTurn, remainingPredictionSize: %i    %i" % (currentTurn, remainingPredictionSize))
-            print("Done init()\n")
+            return biased_roshambo (1 / 3.0, 1 / 3.0), 1 / 3.0
+        
+        oppMove = rps.enemyHistory(currentTurn)
+        
+        if (oppMove == 0):
+            self.statR += 1
+        if (oppMove == 1):
+            self.statP += 1
+        if (oppMove == 2):
+            self.statS += 1
 
-        return biased_roshambo (1 / 3.0, 1 / 3.0), 1 / 3.0
-    
-    oppMove = rps.enemyHistory(currentTurn)
-    
-    if (oppMove == 0):
-        statR += 1
-    if (oppMove == 1):
-        statP += 1
-    if (oppMove == 2):
-        statS += 1
-
-    if (remainingPredictionSize <= 0):
-        if Debug:
-            print ("***********************")
-            print ("Recomputing remainingPredictionSize")
-            print ("***********************")
+        if (self.remainingPredictionSize <= 0):
+            if Debug:
+                print ("***********************")
+                print ("Recomputing remainingPredictionSize")
+                print ("***********************")
+                
+            self.recomputeFutureProb(currentTurn, targetPredictionSize)
             
-        recomputeFutureProb(currentTurn, targetPredictionSize)
+        thisTurnProbR, thisTurnProbP = self.calculateProbThisTurn()
         
-    thisTurnProbR, thisTurnProbP = calculateProbThisTurn()
-    
-    if Debug:
-        print("currentTurn, remainingPredictionSize: %i    %i" % (currentTurn, remainingPredictionSize))
-        print("statR, statP:                         %i    %i" % (statR, statP));
-        print("probR, probP:                         %.2f %.2f" % (probR, probP));
-        print("predictionR, predictionP:             %.2f %.2f" % (predictionR, predictionP));
-        print("thisTurnProbR, thisTurnProbP:         %.2f %.2f" % (thisTurnProbR, thisTurnProbP))
-        debugRecomputation = False
-        
-    if (thisTurnProbR < 0 or thisTurnProbP < 0 or thisTurnProbR + thisTurnProbP > 1.0):
         if Debug:
-            debugRecomputation = True
-            print ("***********************")
-            print ("Recomputing because of", thisTurnProbR <= 0, thisTurnProbP <= 0, thisTurnProbR + thisTurnProbP >= 1.0)
-            print ("***********************")
+            print("currentTurn, remainingPredictionSize: %i    %i" % (currentTurn, self.remainingPredictionSize))
+            print("statR, statP:                         %i    %i" % (self.statR, self.statP));
+            print("probR, probP:                         %.2f %.2f" % (self.probR, self.probP));
+            print("predictionR, predictionP:             %.2f %.2f" % (self.predictionR, self.predictionP));
+            print("thisTurnProbR, thisTurnProbP:         %.2f %.2f" % (thisTurnProbR, thisTurnProbP))
+            debugRecomputation = False
             
-        recomputeFutureProb(currentTurn, targetPredictionSize)
-        thisTurnProbR, thisTurnProbP = calculateProbThisTurn()
+        if (thisTurnProbR < 0 or thisTurnProbP < 0 or thisTurnProbR + thisTurnProbP > 1.0):
+            if Debug:
+                debugRecomputation = True
+                print ("***********************")
+                print ("Recomputing because of", thisTurnProbR <= 0, thisTurnProbP <= 0, thisTurnProbR + thisTurnProbP >= 1.0)
+                print ("***********************")
+                
+            self.recomputeFutureProb(currentTurn, targetPredictionSize)
+            thisTurnProbR, thisTurnProbP = self.calculateProbThisTurn()
 
-    # These two checks will never be True as proven by studying the algorithm. 
-    # But in the interest of keeping this program bug free (and for the sanity of the coder), they are added
-    thisTurnProbR = 0 if thisTurnProbR < 0 else thisTurnProbR
-    thisTurnProbP = 0 if thisTurnProbP < 0 else thisTurnProbP
-    
-    if Debug and debugRecomputation:
-        print("predictionR, predictionP:             %.2f %.2f" % (predictionR, predictionP));
-        print("thisTurnProbR, thisTurnProbP:         %.2f %.2f\n" % (thisTurnProbR, thisTurnProbP))
-    
-    if Debug:
-        input()
+        # These two checks will never be True as proven by studying the algorithm. 
+        # But in the interest of keeping this program bug free (and for the sanity of the coder), they are added
+        thisTurnProbR = 0 if thisTurnProbR < 0 else thisTurnProbR
+        thisTurnProbP = 0 if thisTurnProbP < 0 else thisTurnProbP
         
-    remainingPredictionSize -= 1
-    move = biased_roshambo (thisTurnProbR, thisTurnProbP)
-    
-    # get our confidence of selectred move
-    if move == 0:    confidence = thisTurnProbR
-    elif move == 1:  confidence = thisTurnProbP
-    else:            confidence = 1.0 - (thisTurnProbR + thisTurnProbP)
-    
-    return move, confidence
+        if Debug and debugRecomputation:
+            print("predictionR, predictionP:             %.2f %.2f" % (self.predictionR, self.predictionP));
+            print("thisTurnProbR, thisTurnProbP:         %.2f %.2f\n" % (thisTurnProbR, thisTurnProbP))
+        
+        if Debug:
+            input()
+            
+        self.remainingPredictionSize -= 1
+        move = biased_roshambo (thisTurnProbR, thisTurnProbP)
+        
+        # get our confidence of selectred move
+        if move == 0:    confidence = thisTurnProbR
+        elif move == 1:  confidence = thisTurnProbP
+        else:            confidence = 1.0 - (thisTurnProbR + thisTurnProbP)
+        
+        return move, confidence
 
-def play(targetPredictionSize):
-    move, confidence = BFP(targetPredictionSize)
-    
-    return (move, confidence)
+    def play(self, targetPredictionSize):
+        targetPredictionSize = int(targetPredictionSize)
+        move, confidence = self.predict(targetPredictionSize)
+        
+        return (move, confidence)
