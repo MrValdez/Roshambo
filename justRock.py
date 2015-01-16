a5f288823988364d21ec1a35d5a7f5e1bd22e6f6
MrValdez <shunyvaldez@gmail.com>
2015-01-16 23:21:48 +0800

    forgot to commit 'just rock' prediction module

---
+++ justRock.py
@@ -0,0 +1,17 @@
+# Just play rock
+
+class Rock:
+    def __init__(self, variant):        
+        pass
+
+    def reset(self):
+        pass
+        
+    def update(self):
+        pass
+        
+    def play(self):
+        return 0, 1
+        
+    def init(self, a):
+        pass
