0020af26deb6ea8b4df9d7af1fedee77191f0816
MrValdez <shunyvaldez@gmail.com>
2015-01-16 15:07:16 +0800

    added Latex output to trainer

--- trainer.py
+++ trainer.py
@@ -23,7 +23,12 @@ def main():
     for file in files:
         PlayTournament(file)
         
-#    CreateCSV()
+    print ("")
+    
+    for file in files:
+        CreateLatex(file)
+
+#    CreateCSV(path_output + "data.txt")
 #    charts.startPlotting()
 
 def Validate(filename):
@@ -83,7 +88,7 @@ def GetRank(text, header):
     
     return rank
 
-def CreateCSV(outputFilename = "results"):
+def CreateCSV(output_filename = None):
     """Create CSV by looking at the rank of the Yomi AI"""
     csv = ["", ""]
     fileList = sorted(os.listdir(path_output))
@@ -94,12 +99,12 @@ def CreateCSV(outputFilename = "results"):
     for filename in fileList:
         if filename[-4:] != ".txt":
             print("%s is not txt file" % (filename))
-        
+                
         start = filename.find("[")
         end = filename.find("]")
         variable = filename[start:end + 1]
             
-        with open(pathbase + filename) as f:
+        with open(path_output + filename) as f:
             # find the rank of the Yomi AI            
             text = f.read()
 
@@ -130,13 +135,93 @@ def CreateCSV(outputFilename = "results"):
     print ("Best Match Result      is variant [%s] with rank of %i" % (best[0][0], best[0][1]))
     print ("Best Tournament Result is variant [%s] with rank of %i" % (best[1][0], best[1][1]))
 
-    file = "%s_%s.csv" % (outputFilename, "match")
-    with open(file, "w") as f:
-        f.write(csv[0])
+    if outputFilename != None:
+        file = "%s_%s.csv" % (outputFilename, "match")
+        with open(file, "w") as f:
+            f.write(csv[0])
+        
+        file = "%s_%s.csv" % (outputFilename, "tournament")
+        with open(file, "w") as f:
+            f.write(csv[1])
+
+def CreateLatex(filename):      
+    print ("Building latex for %s." % (filename))
+
+    title = filename.split(".")[0]
+    text = r"""The full results are found at Table \ref{table:%s_results}.
     
-    file = "%s_%s.csv" % (outputFilename, "tournament")
+\begin{table*}
+    \caption{%s results}
+    \label{table:%s_results}
+    \centering
+    \begin{tabular}{|l|c|c|c|c|}
+        \hline
+        \textbf{Vs bot} & \textbf{Total score} & \textbf{Wins} & \textbf{Losts} & \textbf{Ties} \\ \hline
+""" % (title, title, title)
+
+    with open(path_output + filename) as f:
+        f.readline()
+        f.readline()
+        
+        while True:
+            line = f.readline()
+            splitPos = line.find("vs")
+            
+            if splitPos == -1:
+                break
+            
+            line = line[splitPos + 2:]
+            bot, score = line.split("Match:")
+            bot = bot.strip(" * ")
+            bot = bot.strip()
+            score = score.strip()
+            
+            total, score = score.split("(")
+            wins, score  = score.split("+")
+            losts, score = score.split("-")
+            ties, score = score.split("=")
+            
+            total = total.strip()
+            wins  = wins.strip()
+            losts = losts.strip()
+            ties  = ties.strip()
+            
+            text += "%s & %s & %s & %s & %s \\\\ \\hline \n" % (bot, total, wins, losts, ties)
+
+        text = text.strip() + """
+        \end{tabular}
+    \end{table*}"""
+
+        line = None
+        while True:
+            line = f.readline()
+            if line.find("Yomi AI") >= 0:
+                break
+        TournamentRank, score = line.split("Yomi AI")
+        TournamentRankPoints = score.strip().split(" ")[0]
+        
+        line = None
+        while True:
+            line = f.readline()
+            if line.find("Yomi AI") >= 0:
+                break
+        MatchRank, score = line.split("Yomi AI")
+        score = score.split(" ")
+        score = [spam for spam in score if spam != ""]
+        MatchRankTotals, MatchRankWins, MatchRankLosts, MatchRankDraws = score[0:4]
+        
+#        text += "\n\nTournament Rank: %s (%s)\nMatch Rank: %s (%s) (%s+ %s- %s=)" % \
+#                (TournamentRank.strip(), TournamentRankPoints, 
+#                 MatchRank.strip(), MatchRankTotals, MatchRankWins, MatchRankLosts, MatchRankDraws)
+
+        text += "\n\n"
+        text += "The Match rank is %s with a total points of %s (%s win, %s losts and %s ties). " % (MatchRank.strip(), MatchRankTotals, MatchRankWins, MatchRankLosts, MatchRankDraws)
+        text += "The Tournament rank is %s with %s points." % (TournamentRank.strip(), TournamentRankPoints)
+
+    file = path_output + title + ".tex"
     with open(file, "w") as f:
-        f.write(csv[1])
-      
+        f.write(text)
+
+  
 if __name__ == "__main__":
     main()
