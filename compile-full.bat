4e51771388a8d6419130487b436bf1b8017db1f8
MrValdez <shunyvaldez@gmail.com>
2015-02-13 09:04:14 +0800

    added SIMD oriented Fast Mersenne Twister(SFMT). Please check LICENSE for information on where this library was taken from.

--- compile-full.bat
+++ compile-full.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
 
-gcc -w -m64 rsb-ts1-full.c python.c -lm -I/Python34/include/ -Lpython_c_api/ -lpython -o full.exe
+gcc -w -m64 rsb-ts1-full.c python.c SFMT-src-1.4.1/SFMT.c -lm -I/Python34/include/ -ISFMT-src-1.4.1/ -Lpython_c_api/ -lpython -o full.exe
 if %ERRORLEVEL% NEQ 1 full.exe
